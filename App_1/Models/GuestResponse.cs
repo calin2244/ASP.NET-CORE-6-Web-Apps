e7fe972706f16bfb8dbe2fd90a2c745c9705d2ae
Calin Basturea <bastureacalin@gmail.com>
2022-07-27 21:19:47 +0300

    The web app

---
+++ App_1/Models/GuestResponse.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using PartyInvites.Scripts;
+
+namespace PartyInvites.Models
+{
+    public class GuestResponse
+    {
+        [Required(ErrorMessage = "● Please enter your name")]
+        [CustomAnnotations.ExcludeChar("#!#?><.,:@%^&", ErrorMessage = "● Name contains invalid character")]
+        public string? Name { get; set; }
+
+        [Required(ErrorMessage = "● Please enter your email address")]
+        [CustomAnnotations.HasAt("@", ErrorMessage = "● Incorrect formatting for the email address")]
+        public string? Email { get; set; }
+
+        [Required(ErrorMessage = "● Please enter your phone number")]
+        [CustomAnnotations.ExcludeChar("abcdefhijklmnopqrstuvwxyz", ErrorMessage = "● A phone number can't contain any letters")]
+        public string? Phone { get; set; }
+
+        [Required(ErrorMessage = "● Please tell me whether you're coming or not")]
+        public bool? WillAttend { get; set; }
+    }
+}
---
+++ App_1/Scripts/CustomAnnotations.cs
@@ -0,0 +1,62 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace PartyInvites.Scripts
+{
+    public class CustomAnnotations
+    {
+        public class ExcludeChar : ValidationAttribute
+        {
+            private readonly string chars;
+            public ExcludeChar(string chars) : base("{0} contains invalid characters")
+            {
+
+                this.chars = chars;
+            }
+
+            protected override ValidationResult IsValid(object? value, ValidationContext validationContext)
+            {
+                if (value != null)
+                {
+                    for (int i = 0; i < chars.Length; ++i)
+                    {
+                        var valueAsString = value.ToString();
+                        if (valueAsString != null && valueAsString.Contains(chars[i]))
+                        {
+                            var errorMsg = FormatErrorMessage(validationContext.DisplayName);
+                            return new ValidationResult(errorMsg);
+                        }
+                    }
+                }
+                return ValidationResult.Success;
+            }
+
+            
+        }
+
+        public class HasAt : ValidationAttribute
+        {
+            private readonly string chars;
+            public HasAt(string chars) : base("{0} contains invalid characters")
+            {
+                this.chars = chars;
+            }
+
+            protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+            {
+                if(value != null)
+                {
+                    var valAsString = value.ToString();
+                    for(int i = 0; i < chars.Length; ++i)
+                    {
+                        if(valAsString != null && !valAsString.Contains(chars[i]))
+                        {
+                            var errMsg = FormatErrorMessage(validationContext.DisplayName);
+                            return new ValidationResult(errMsg);
+                        }
+                    }
+                }
+                return ValidationResult.Success;
+            }   
+            
+        }
+    }
+}
